55e0d4b333fba51e4b2acc6afb3cc48029d29b5d
OchaThicc <slee397@ucsc.edu>
2024-10-17 18:07:12 -0700

    Made index and Jss for pseudocode

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors - PseudoCode Implementation
+// Author: Sean Lee
+// Date: 10/17/2024
 
 // Constants
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//Assign a player input box, which they can only choose rock paper or scissors
+//Make a randomizer to have the ai choose any element
+  //Accept Player input
+    //Compare player input to computer chosen input
+      //Test win conditions
+      //Win cons are same element = tie
+      //Rock beats scissors
+      //Paper beats rock
+      //Scissors beat paper
+//Depending on win condition display “You win” or “Tie” or “You lose”
